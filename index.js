94f98690da6d9ebec59458cb8c56629fb77472f7
Andy Choi <wooseokchoi96@gmail.com>
2019-09-15 18:12:56 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file
+function findMatching(drivers, nameInput) {
+    return drivers.filter(
+        function(name) {
+            return name.toLowerCase() === nameInput.toLowerCase();
+        }
+    );
+}
+
+function fuzzyMatch(drivers, firstFewLetters) {
+    const searchLength = firstFewLetters.length;
+    return drivers.filter(
+        function(name) {
+            return name.toLowerCase().slice(0, searchLength) === firstFewLetters.toLowerCase();
+        }
+    );
+}
+
+function matchName(drivers, nameInput) {
+    return drivers.filter(
+        function(driver) {
+            return driver.name.toLowerCase() === nameInput.toLowerCase();
+        }
+    );
+}
