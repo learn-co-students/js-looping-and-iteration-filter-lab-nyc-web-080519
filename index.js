d9c4d50d86ca90387ea67b657cd638e832822003
github username <github email address>
2019-09-16 03:07:38 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
-// Code your solution in this file
+function findMatching(drivers, string) {
+    let namedDrivers = []
+
+    for (const driver of drivers) {
+        if (driver.toUpperCase() === string.toUpperCase()) {
+            namedDrivers.push(driver)
+        }
+    }
+    return namedDrivers
+}
+
+function fuzzyMatch(drivers, segment) {
+    //determine how long the start of the name is
+    let nameLength = segment.length;
+    //returns just the names taht start with the section
+    return drivers.filter(function (driverName) {
+        // the section is determined using a slice method and the found length
+        return driverName.slice(0, nameLength) === segment;
+    });
+}
+
+function matchName(drivers, name) {
+    return drivers.filter(function (driver) {
+        return driver.name.toUpperCase() === name.toUpperCase();
+    });
+}
