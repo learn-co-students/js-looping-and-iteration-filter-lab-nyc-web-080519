3ab428c66c1857fe775e71b6faf50cc703fb2e18
Mason Balbera <mhbalbera@gmail.com>
2019-09-16 09:41:47 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution in this file
+
+
+
+function findMatching(drivers, str){
+    return drivers.filter(function (d) {
+        return d.toLowerCase() === str.toLowerCase();
+    });
+}
+
+function fuzzyMatch(drivers, str){
+    return drivers.filter(function (d) {
+        return d.slice(0, str.length) === str;
+    });
+}
+
+function matchName(drivers, str) {
+    return drivers.filter(function(d) {
+        return d.name.toUpperCase() === str.toUpperCase();
+    });
+}
